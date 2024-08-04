65af51711b3ede1ba5831d643acc0de206e4979b
Natalia Zelenevskaya <zelenevskayan2015@gmail.com>
2024-08-04 20:03:42 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let openedBracketsArray = [];
+  let closedBracketsArray = [];
+  let equalBracketsArray = [];
+
+  let stack = [];
+
+  for (let item of bracketsConfig) {
+    openedBracketsArray.push(item[0]);
+  }
+
+  for (let item of bracketsConfig) {
+    closedBracketsArray.push(item[1]);
+  }
+
+  for (let item of bracketsConfig) {
+    if (item[0] === item[1]) {
+      equalBracketsArray.push(item[0]);
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (openedBracketsArray.includes(str[i])) {
+      stack.push(str[i]);
+
+      if (
+        stack.length > 1 &&
+        equalBracketsArray.includes(stack[stack.length - 1]) &&
+        equalBracketsArray.includes(stack[stack.length - 2]) &&
+        equalBracketsArray.indexOf(stack[stack.length - 1]) ===
+          equalBracketsArray.indexOf(stack[stack.length - 2])
+      ) {
+        stack.pop();
+        stack.pop();
+      }
+    } else if (
+      !openedBracketsArray.includes(str[i]) &&
+      stack.length > 0 &&
+      openedBracketsArray.indexOf(stack[stack.length - 1]) ===
+        closedBracketsArray.indexOf(str[i])
+    ) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
